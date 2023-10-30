a754e0bc697030d75209a456ce60cab0310ef559
Luiz Cardoso <luizcarloscardoso2010@gmail.com>
2023-10-30 11:05:39 -0300

    :lipstick: change heart color

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function toggleMode() {
   const html = document.documentElement;
   const img = html.querySelector("#profile img");
+  const footer = html.querySelector("footer");
   const lightMode = "light";
 
   html.classList.toggle(lightMode);
 
   if (html.classList.contains(lightMode)) {
     img.setAttribute("src", "./resources/logo-light.png");
+    footer.innerHTML = "Feito com ❤️ por © Chaves & Borba";
   } else {
     img.setAttribute("src", "./resources/logo-dark.png");
+    footer.innerHTML = "Feito com 🤍 por © Chaves & Borba";
   }
 }
